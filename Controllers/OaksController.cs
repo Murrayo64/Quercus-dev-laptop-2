83280098abb6303caa5861445582beb2bc8e502f
Joe Murray <jmurray45@gmail.com>
2024-09-11 10:40:38 -0400

    first commit

---
+++ Controllers/OaksController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Quercus.Models;
+
+namespace Quercus.Controllers{
+
+    public class OaksController : Controller{
+
+        private TreeContext _context {get; set;} 
+
+        public OaksController(TreeContext ctx) {
+            _context = ctx;
+        }
+
+        public IActionResult List(){
+           var oaks  = _context.Oaks.ToList().Take(50);
+           return View(oaks);
+        }
+    }
+
+
+
+}
